c56cf170a05a9175b028356a6df8f653aa965619
Ethan Mohr <ethanmohr2000@gmail.com>
2026-01-23 17:23:52 -0700

    Session 3: Pin cell simulation

--- 00_admin/session_log.md
+++ 00_admin/session_log.md
@@ -22,3 +22,16 @@ What I did:
 
 Next:
 - Simple pin cell simulations in hyper-robust environment
+
+## 2026-01-20 | Session 3
+
+Goal:
+- Set up pin cell simulation
+
+What I did:
+- Fixed VSCode SSH setup with straightedge
+- Tested structure created in last session
+- Created pin cell geometry and ran simulation on straightedge successfully
+
+Next:
+- More complex geometry and transition to optimization problems
--- 02_simulation/build_model.py
+++ 02_simulation/build_model.py
@@ -2,27 +2,72 @@
 import openmc
 
 def build_model(config):
-    # Simplest possible material for testing
-    fuel = openmc.Material(name="fuel")
-    fuel.set_density("g/cm3", 10.0)
+    # Get variables from the config file
+    yml_geom = config["geometry"]
+    yml_sett = config["settings"]
 
-    fuel.add_nuclide("U235", 1.0)
-    fuel.add_nuclide("U238", 4.0)
+    # UO2, cladding, and water for basic testing
+    uo2 = openmc.Material(name="UO2")
+    uo2.set_density("g/cm3", 10.4)
 
-    materials = openmc.Materials([fuel])
+    uo2.add_nuclide("U235", 1.0)
+    uo2.add_nuclide("U238", 4.0)
+    uo2.add_element("O", 10.0)
+
+    water = openmc.Material(name="Water")
+    water.set_density("g/cm3", 1.0)
+
+    water.add_element("H", 2.0)
+    water.add_element("O", 1.0)
+
+    cladding = openmc.Material(name="Zirconium cladding")
+    cladding.set_density("g/cm3", 6.55)
+
+    cladding.add_element("Zr", 1.0)
+
+    materials = openmc.Materials([uo2, water, cladding])
+
+    # Define a simple pin cell geometry
+    fuel_radius = yml_geom["fuel_radius"]
+    clad_inner_radius = yml_geom["clad_inner_radius"]
+    clad_outer_radius = yml_geom["clad_outer_radius"]
+    pitch = yml_geom["pitch"]
+    half_pitch = pitch / 2
 
     # Simplest possible volume for testing
-    xmin = openmc.XPlane(x0=-50.0, boundary_type="reflective")
-    xmax = openmc.XPlane(x0= 50.0, boundary_type="reflective")
-    ymin = openmc.YPlane(y0=-50.0, boundary_type="reflective")
-    ymax = openmc.YPlane(y0= 50.0, boundary_type="reflective")
-    zmin = openmc.ZPlane(z0=-50.0, boundary_type="reflective")
-    zmax = openmc.ZPlane(z0= 50.0, boundary_type="reflective")
+    xmin = openmc.XPlane(x0=-half_pitch, boundary_type="reflective")
+    xmax = openmc.XPlane(x0= half_pitch, boundary_type="reflective")
+    ymin = openmc.YPlane(y0=-half_pitch, boundary_type="reflective")
+    ymax = openmc.YPlane(y0= half_pitch, boundary_type="reflective")
 
-    region = +xmin & -xmax & +ymin & -ymax & +zmin & -zmax
+    fuel_surf = openmc.ZCylinder(r=fuel_radius)
+    clad_inner_surf = openmc.ZCylinder(r=clad_inner_radius)
+    clad_outer_surf = openmc.ZCylinder(r=clad_outer_radius)
 
-    cell = openmc.Cell(name="infinite_medium", fill=fuel, region=region)
-    geometry = openmc.Geometry([cell])
+    fuel_region = (
+        -fuel_surf
+        & +xmin & -xmax
+        & +ymin & -ymax
+    )
+
+    clad_region = (
+        +fuel_surf
+        & -clad_outer_surf
+        & +xmin & -xmax
+        & +ymin & -ymax
+    )
+
+    water_region = (
+        +clad_outer_surf
+        & +xmin & -xmax
+        & +ymin & -ymax
+    )
+
+    fuel_cell = openmc.Cell(name="fuel_cell", fill=uo2, region=fuel_region)
+    clad_cell = openmc.Cell(name="clad_cell", fill=cladding, region=clad_region)
+    water_cell = openmc.Cell(name="water_cell", fill=water, region=water_region)
+
+    geometry = openmc.Geometry([fuel_cell, clad_cell, water_cell])
 
     # Eigenvalue mode and simulation settings
     settings = openmc.Settings()
@@ -43,7 +88,6 @@ def build_model(config):
 
     # Add simplest possible tally for testing
     tally = openmc.Tally(name="flux")
-    tally.filters = [openmc.CellFilter(cell)]
     tally.scores = ["flux"]
 
     tallies = openmc.Tallies([tally])
